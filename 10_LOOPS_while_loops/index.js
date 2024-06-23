563e77bdbd5fc5acae8da4a22d2ff6906fd93105
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-06-24 01:13:19 +0530

    Create index.js
    while and do-while loops in javascript

---
+++ 10_LOOPS_while_loops/index.js
@@ -0,0 +1,21 @@
+//while loop:-------->
+let n = prompt("Enter the value of n:")
+n = Number.parseInt(n)
+
+let i=0;                   //output: 0 1 2 
+while(i<n){
+    console.log(i);
+     i++; // gar i++ nhi lagayenge toh 0 infinite tk print hota rahega
+}
+//if we write (true) instead of (i<n) code will execute endlessly
+
+
+//do-while loop:-------->
+let m = prompt("Enter the value of n:")
+n = Number.parseInt(m)
+
+let j=10;   //kaam se kaam ek baar execute hota hai
+do{
+    console.log(j)
+    j++;
+}while(j<m)
