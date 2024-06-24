a1ce786b527b9175951d38cab58962af4b87c700
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-06-25 01:19:18 +0530

    Create index.js
    string methods in javascript

---
+++ 14 Strings 02/index.js
@@ -0,0 +1,32 @@
+//(1)length of string
+let name="Niraj"
+console.log(name.length)
+
+//(2)uppercase-lowercase
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+
+//(3)slice
+console.log(name.slice(2,4))//------>2 se 4 tak jisme 4 is not included
+console.log(name.slice(2))
+
+//(4)replace
+console.log(name.replace("Ni","Su"))
+
+//(5)concat method
+let friend="Nitin"
+console.log(name.concat("is a friend of",friend,"Ok"))
+
+//(6)trim method
+let friend2="   Meena        "
+console.log(friend2)
+console.log(friend2.trim())
+console.log(friend2.trim)
+ 
+let nm = "Shandhya" + "Yadav" + "Jaunpur"
+console.log(nm)
+console.log(nm[0])
+console.log(nm[1])
+console.log(nm[2])
+console.log(nm[3])
+console.log(nm[4])
