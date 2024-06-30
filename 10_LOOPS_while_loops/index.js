13b3ce0314a5fd12304f39fa6ba3407549fe3bea
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-06-30 23:57:49 +0530

    Update index.js

--- 10_LOOPS_while_loops/index.js
+++ 10_LOOPS_while_loops/index.js
@@ -5,7 +5,7 @@ n = Number.parseInt(n)
 let i=0;                   //output: 0 1 2 
 while(i<n){
     console.log(i);
-     i++; // gar i++ nhi lagayenge toh 0 infinite tk print hota rahega
+     i++; // agar i++ nhi lagayenge toh 0 infinite tk print hota rahega
 }
 //if we write (true) instead of (i<n) code will execute endlessly
 
