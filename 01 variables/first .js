c85bc797563402587a97a42d451e874e55d3b4fc
Niraj <whitedevil2188@gmail.com>
2024-07-01 22:16:05 +0530

    variables updated

--- 01 variables/first .js
+++ 01 variables/first .js
@@ -1,4 +1,12 @@
 //JavaScript is a programming language used for creating dynamic content on websites.
 
+//Client-side: It supplies objects to control a browser and its Document Object Model (DOM).
+//Server-side: It supplies objects relevant to running JavaScript on a server
+
+//It was created in 1995 by *Brendan Eich* while he was an engineer at *Netscape*
+
+
 let a = 69
 console.log(a)
+
+console.log("Hello World! I Don't Give a Bug");
