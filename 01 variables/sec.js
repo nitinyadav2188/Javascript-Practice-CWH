50cdaab4b48caeb8eebbc6d35fb9b4b08f0acc53
Niraj <whitedevil2188@gmail.com>
2024-07-01 22:22:06 +0530

    added

--- 01 variables/sec.js
+++ 01 variables/sec.js
@@ -1,3 +1,12 @@
+//JavaScript is a programming language used for creating dynamic content on websites.
+
+//Client-side: It supplies objects to control a browser and its Document Object Model (DOM).
+//Server-side: It supplies objects relevant to running JavaScript on a server
+
+//It was created in 1995 by *Brendan Eich* while he was an engineer at *Netscape*
+//JavaScript is considered lightweight due to the fact that it has low CPU usage, is easy to implement, and has a minimalist syntax
+//
+
 console.log("Javascript tutorial 3: var let const")
 var a= 45;
 var b= "Harry";
