b0c50ad71a57c059493e9ae2d8e484e0a5137e42
Niraj <whitedevil2188@gmail.com>
2024-07-01 22:20:19 +0530

    add

--- 01 variables/first .js
+++ 01 variables/first .js
@@ -4,7 +4,8 @@
 //Server-side: It supplies objects relevant to running JavaScript on a server
 
 //It was created in 1995 by *Brendan Eich* while he was an engineer at *Netscape*
-
+//JavaScript is considered lightweight due to the fact that it has low CPU usage, is easy to implement, and has a minimalist syntax
+//
 
 let a = 69
 console.log(a)
