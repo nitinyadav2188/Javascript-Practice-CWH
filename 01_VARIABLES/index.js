358e7f8502163899598b4e30d1a77b2c12d241e3
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-07-01 23:04:17 +0530

    Update index.js with new information

--- 01_VARIABLES/index.js
+++ 01_VARIABLES/index.js
@@ -1,4 +1,46 @@
-//JavaScript is a programming language used for creating dynamic content on websites.
+/*JavaScript is a programming language used for creating dynamic content on websites.*/
+
+//Client-side: It supplies objects to control a browser and its Document Object Model (DOM).
+//Server-side: It supplies objects relevant to running JavaScript on a server
+
+//It was created in 1995 by *Brendan Eich* while he was an engineer at *Netscape*
+//JavaScript is considered lightweight due to the fact that it has low CPU usage, is easy to implement, and has a minimalist syntax
+//A lightweight language does not consume much of your CPU’s resources. It doesn’t put excess strain on your CPU or RAM
+
+/*~~~~~~~~~~~~Adding JavaScript in HTML Document:-------> */
+//JavaScript code is placed inside the <head> section of an HTML page and uses the <script> element. This ensures the script is loaded and executed when the page loads.
+
+/*~~~~~~~~~~~~~~~~~Advantages of External JavaScript:------>*/
+// Cached JavaScript files can speed up page loading.
+// It makes JavaScript and HTML easier to read and maintain.
+// It separates the HTML and JavaScript code.
+// It focuses on code reusability which is one JavaScript Code that can run in various HTML files.
+
+/*~~~~~ Keywords: ------>
+Keywords are reserved words and cannot be used as a variable name. A JavaScript keyword tells about what kind of operation it will perform.*/
+// var:Used to declare a variable
+// let:Used to declare a block variable
+// const:Used to declare a block constant
+// if:Used to decide if certain block will get executed or not
+// switch:Executes a block of codes depending on different cases
+// for:Executes a block of statements till the condition is true
+// function:Used to declare a function
+// return:Used to exit a function
+// try:Used to handle errors in a block of statements
+// break:Used to terminate a loop
+// continue:Used to continue a loop
+
+/*~~~~~~JavaScript Identifiers:----->
+JavaScript Identifiers are names used to name variables and keywords and functions.*/
+//A identifier must begin with:
+//A letter(A-Z or a-z)
+//A dollar sign($)
+//A underscore(_)
+//Note: Numbers are not allowed as a first character in JavaScript Identifiers.
+
+
+/*~~~~~Variables:---->
+In JavaScript, variables are used to store and manage data. They are created using the var, let, or const keyword.*/
 
 console.log("Javascript tutorial 3: var let const")
 var a= 45;
@@ -6,7 +48,13 @@ var b= "Harry";
 var c = null;
 var d = undefined
 {
-    let b = 'this is good'
+    let b = 'this'
     console.log(b)
 }
 console.log(b)
+
+/*~~~~~Global Variables:------>
+Global variables are those defined outside of any function, making them accessible from anywhere within the script.*/
+
+/*~~~~~Local Variables:------->
+Local variables are defined within functions in JavaScript. They are confined to the scope of the function that defines them and cannot be accessed from outside.*/
