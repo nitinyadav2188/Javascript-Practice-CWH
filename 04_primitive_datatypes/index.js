038fb983dfb0ed1af7444485dbd9f642fbd203e6
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-07-01 23:07:11 +0530

    Update index.js

--- 04_primitive_datatypes/index.js
+++ 04_primitive_datatypes/index.js
@@ -1,4 +1,6 @@
-//--PRIMITIVE DATATYPE--
+//--PRIMITIVE DATATYPE-->
+//The predefined data types provided by JavaScript language are known as primitive data types
+
 let a = null;
 let b = 345;
 let c = true;
@@ -12,6 +14,9 @@ console.log(a,b,c,d,e,f,g)
 console.log(typeof f); 
 console.log(typeof c); 
 console.log(typeof h); 
+
+/*~~~~~~~Non-Primitive Data Types:--------->
+The data types that are derived from primitive data types of the JavaScript language are known as non-primitive data types. It is also known as derived data types or reference data types.*/
 
 //--OBJECTS in JS--
 const item={
