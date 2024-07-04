78c38c98ccfd3e3af2b5d22279dbe731401c476e
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-07-04 23:55:50 +0530

    Add files via upload

---
+++ Javascript Form Validation/Form-01/script.js
@@ -0,0 +1,22 @@
+function validate() {
+    var name = document.f1.name.value;
+    var password = document.f1.password.value;
+    var status = false;
+
+    if (name.length < 1) {
+        document.getElementById("nameloc").innerHTML =
+            " <img src='unchecked.gif'/> Please enter your name";
+        status = false;
+    } else {
+        document.getElementById("nameloc").innerHTML = " <img src='checked.gif'/>";
+        status = true;
+    }
+    if (password.length < 6) {
+        document.getElementById("passwordloc").innerHTML =
+            " <img src='unchecked.gif'/> Password must be at least 6 char long";
+        status = false;
+    } else {
+        document.getElementById("passwordloc").innerHTML = " <img src='checked.gif'/>";
+    }
+    return status;
+}
