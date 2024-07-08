fffb490297f9ece148fa077a71e6d3353261311b
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-07-08 20:18:44 +0530

    Update index.js

--- 11_Functions/index.js
+++ 11_Functions/index.js
@@ -1,5 +1,13 @@
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~FUNCTIONS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+
+/* Advantage of JavaScript function:-
+ There are mainly two advantages of JavaScript functions.
+1) Code reusability: We can call a function several times so it save coding.
+2) Less coding: It makes our program compact. We don’t need to write many lines of code each time to perform a common task.
+*/
+
+
 function onePlusAvg(x,y){
     console.log("Done!!")
     //return (x+y)/2
