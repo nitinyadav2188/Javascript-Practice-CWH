a475d8122832b6c8d5548a5b6a481bb41705b823
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-07-14 10:09:39 +0530

    Add files via upload

---
+++ 36 Searching the DOM/index.js
@@ -0,0 +1,10 @@
+//change card title to red:
+let ctitle = document.getElementById("firstcardtitle")
+ctitle.style.color ="blue"
+
+
+let ctitle = document.querySelectorAll(".card-title")
+ctitle[0].style.color="red"
+ctitle[1].style.color="blue"
+ctitle[2].style.color="green"
+console.log(ctitle)
