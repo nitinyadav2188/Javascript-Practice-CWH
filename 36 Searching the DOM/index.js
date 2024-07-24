619bfafde63c431ed4bb806ab328f2824bb4abe9
Niraj <whitedevil2188@gmail.com>
2024-07-24 22:12:49 +0530

    updated

--- 36 Searching the DOM/index.js
+++ 36 Searching the DOM/index.js
@@ -8,3 +8,12 @@ ctitle[0].style.color="red"
 ctitle[1].style.color="blue"
 ctitle[2].style.color="green"
 console.log(ctitle)
+
+// The selector methods accept selectors to determine what element or elements should be returned.
+
+/* Element.querySelector() method:--->
+The querySelector() method of the Element interface returns the first element that is a descendant of the element on which it is invoked that matches the specified group of selectors.
+
+
+
+*/
--- 37 matches closest contain/index.js
+++ 37 matches closest contain/index.js
@@ -2,10 +2,16 @@
 let id1 = document.getElementById("id1")
 let sp1 = document.getElementById("sp1")
 console.log(id1)
-
+/* Matches():--->The matches() method of the Element interface tests whether the element would be selected by the specified CSS selector.
+               true if the Element matches the selectors. Otherwise, false.
+*/
 console.log(id1.matches(".class"))    //false kyuki class ka asli naam box hai n ki class
 console.log(id1.matches(".box"))      //true
 
+/* Closest()----->The closest() method of the Element interface traverses the element and its parents (heading toward the document root) until it finds a node that matches the specified CSS selector.
+                  The closest ancestor Element or itself, which matches the selectors. If there are no such element, null.                 
+*/
 console.log(sp1.closest("#sp1"))
 console.log(id1.contains(sp1))       //true
 console.log(sp1.contains(sp1))       //true
+
